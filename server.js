25f8fbc5e47df6e9f43303648091bbd961b1b94c
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-11-24 12:24:53 +0100

    new user post route

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const { Post, User, Comment } = require("./model");
 
 //Launch mongoose
 mongoose
-  .connect("mongodb://localhost:27017/reuddit", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017/reuddit")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -40,7 +39,34 @@ app.get("/post/:postId", async (req, res) => {
 //delete a post
 
 //create user
+app.post("/signin", async (req, res) => {
+  const { username, email, password } = req.body;
 
+  if (!username || !email || !password) {
+    return res.status(400).send("Missing required fields");
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).send("Email already exists");
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+  } catch (err) {
+    res.status(500).send("Error creating user: " + err);
+  }
+});
 //login
 
 //get comments
