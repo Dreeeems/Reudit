4e2114b3e39577f9ef5bb0a5bc96aba7e3d836b7
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-11-24 11:48:08 +0100

    mongoose connexion with local client

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { Post, User, Comment } = require("./model");
+
+//Launch mongoose
+mongoose
+  .connect("mongodb://localhost:27017/reuddit", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 const app = express();
 const port = 3000;
 
